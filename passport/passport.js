180c0d62583e26431b043adbd4a533a427976fcf
Smushed <smushedcode@gmail.com>
2019-01-04 07:20:10 -0600

    Error checking now working on the front end as well as the back end

--- passport/passport.js
+++ passport/passport.js
@@ -40,35 +40,10 @@ module.exports = function (passport, user) {
             let zip = "";
             req.body.zip.trim().replace(/\s+/g, '').split("").forEach(item => { if (!isNaN(item)) { zip += item } });
 
-            //Input Verification
-            const inputChecking = function (password, zip, phone_number) {
-                //Returns the error code which will be used later to break out of this function
-                //TODO Make this an array, logging each of the errors and displaying them at once
-                if (typeof zipcodes.lookup(zip) === "undefined") {
-                    return 1;
-                };
-                if (!password || password.length < 6) {
-                    return 2;
-                };
-                if (phone_number.length != 10) {
-                    return 3
-                };
-            };
-
-            const inputErrors = inputChecking(password, zip, phone_number);
-
-            //Doing this to break out of the signup function before going to submit it to the database
-            if (inputErrors === 1) {
+            //Checks if the zip code inputted is a valid US zip
+            if (typeof zipcodes.lookup(zip) === "undefined") {
                 return done(null, false, {
                     message: "Please input a valid US zip"
-                });
-            } else if (inputErrors === 2) {
-                return done(null, false, {
-                    message: "Please input a password at least 6 characters long"
-                });
-            } else if (inputErrors === 3) {
-                return done(null, false, {
-                    message: "Please input a US phone number ex. 123-456-7890"
                 });
             };
 
--- public/js/passportError.js
+++ public/js/passportError.js
@@ -1,54 +1,47 @@
 $(`.errorMessage`).hide();
 
 const validateSignUp = () => {
-    let phoneInput = $(".phone_number").val();
-    let zipInput = $(".zip").val();
+    //Grabs the user inputs
+    const phoneInput = $(".phone_number").val();
+    const zipInput = $(".zip").val();
+    const passwordInput = $(".password").val();
+
+    //Sets up the phone number and zip for future manipulation
     let phone_number = "";
     let zip = "";
     const errorArray = []; //Error array collects the errors then writes them to the screen
 
-    event.preventDefault();
-
-    if (phoneInput === "") {
-        phoneInput = "BadData";
-    } else {
-        //Getting the phone number & zip code into a standardized format
-        //Converts the inputted number into an array, gets rid of all the spaces and adds only the numbers onto a string
+    //Getting the phone number & zip code into a standardized format
+    //Converts the inputted number into an array, gets rid of all the spaces and adds only the numbers onto a string
+    if (phoneInput != "") {
         phoneInput.trim().replace(/\s+/g, '').split("").forEach(item => { if (!isNaN(item)) { phone_number += item } });
     };
-    if (zipInput === "") {
+    if (zipInput != "") {
         //TODO Test zipcodes on the front end
         //Currently a zip is tested if it's blank on the front end but tested if it's valid on the back end
-        zipInput = "BadData";
-    } else {
         zipInput.trim().replace(/\s+/g, '').split("").forEach(item => { if (!isNaN(item)) { zip += item } });
     };
-    console.log(phoneInput, zipInput, phone_number, zip);
-    // //Returns the error code which will be used later to break out of this function
-    // //TODO Make this an array, logging each of the errors and displaying them at once
-    // if (typeof zipcodes.lookup(zip) === "undefined") {
-    //     return 1;
-    // };
-    // if (!password || password.length < 6) {
-    //     return 2;
-    // };
-    // if (phone_number.length != 10) {
-    //     return 3
-    // };
 
-    // //Doing this to break out of the signup function before going to submit it to the database
-    // if (inputErrors === 1) {
-    //     return done(null, false, {
-    //         message: "Please input a valid US zip"
-    //     });
-    // } else if (inputErrors === 2) {
-    //     return done(null, false, {
-    //         message: "Please input a password at least 6 characters long"
-    //     });
-    // } else if (inputErrors === 3) {
-    //     return done(null, false, {
-    //         message: "Please input a US phone number ex. 123-456-7890"
-    //     });
-    // };
+    //Returns the error code which will be used later to break out of this function
+    //TODO Make this an array, logging each of the errors and displaying them at once
+    if (zip.length != 5 || zipInput === "") {
+        errorArray.push("Zip must be a 5 digit US zip");
+    };
+    if (passwordInput === "" || passwordInput.length < 6) {
+        errorArray.push("Password must be 6 characters long");
+    };
+    if (phone_number.length != 10 || phoneInput === "") {
+        errorArray.push("Phone Number must be 10 digit US Phone Number")
+    };
+
+    //Checks if there are any errors, if so display them all to the screen
+    if (errorArray.length > 0) {
+        const errorDiv = $("<div>");
+        errorArray.forEach(error => {
+            errorDiv.append(`<p>${error}</p>`)
+        });
+        $(".errorMessages").append(errorDiv);
+        event.preventDefault();
+    };
 };
 
